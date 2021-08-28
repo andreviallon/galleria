d6c92fe8436669fb4cbdb788d20520ced577725a
Andre Viallon <viallon.andre@gmail.com>
2021-08-28 17:24:31 +0200

    chore: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      gray: {
+        light: "#F3F3F3",
+        regular: "#E5E5E5",
+        dark: "#7D7D7D",
+      },
+    },
     extend: {},
   },
   variants: {
